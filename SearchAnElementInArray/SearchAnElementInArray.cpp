63a2284467e0e0eec99969ada34d3748fd949012
Ankit Mishra <ankit1703@gmail.com>
2017-04-02 17:06:20 +0530

    All test case passed

---
+++ SearchAnElementInArray/SearchAnElementInArray.cpp
@@ -0,0 +1,35 @@
+// SearchAnElementInArray.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t(0), n, ele, flag(0);
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n;
+		int *arr = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		cin >> ele;
+		for (int i = 0; i < n; i++)
+		{
+			if (ele == arr[i])
+			{
+				cout << i << endl;
+				flag = 1;
+				break;
+			}
+		}
+		if (!flag) cout << -1 << endl;
+	}
+    return 0;
+}
+
