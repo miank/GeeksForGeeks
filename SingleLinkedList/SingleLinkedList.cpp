cb7e177020f836ee0630665e7189f025362c54c4
Ankit Mishra <ankit1703@gmail.com>
2017-04-25 23:41:12 +0530

    Many functions added

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -18,10 +18,17 @@ public:
 		start = nullptr;
 	}
 	node *CreateNode(int data);
+	void Display();
+	void SearchList();
 	void InsertAtBeg();
 	void InsertAtEnd();
+	void InsertBetween(); // Not Implemented
+	void InsertAfterNode();
+	void InsertBeforeNode();
+	void InsertAtPosition();
 };
 
+// Creating the node
 node * LinkedList::CreateNode(int data)
 {
 	node *temp = new node();
@@ -30,6 +37,36 @@ node * LinkedList::CreateNode(int data)
 	return temp;
 }
 
+// Display the contents of the list
+void LinkedList::Display()
+{
+	node *temp = start;
+	while (temp != nullptr)
+	{
+		cout << temp->info << " ";
+		temp = temp->next;
+	}
+}
+
+// Search the list
+void LinkedList::SearchList()
+{
+	int value(0), pos = 1;
+	cout << "Enter the value to seach " << endl;
+	cin >> value;
+
+	node *p = start;
+	while (p != start)
+	{
+		if (p->info == value)
+		{
+			cout << "Value found at " << pos;
+		}
+		pos++;
+	}
+}
+
+// Insert the list at the end
 void LinkedList::InsertAtBeg()
 {
 	int value(0);
@@ -37,24 +74,97 @@ void LinkedList::InsertAtBeg()
 	cin >> value;
 
 	node *temp = CreateNode(value);
-	if (temp == nullptr)
+	if (start == nullptr)
 	{
-		temp = start->next;
-
+		temp->next = nullptr;
+		start = temp;
+		return;
 	}
-
-
-
-
+	else
+	{
+		temp->next = start;
+		start = temp;
+		return;
+	}
 }
 
+// Insert the list at the end
 void LinkedList::InsertAtEnd()
 {
+	int value(0);
+	node *temp(nullptr);
 
+	cout << "Enter the value ";
+	cin >> value;
+	node *newNode = CreateNode(value);
+
+	temp = start;
+	while (temp != nullptr)
+	{
+		temp = temp->next;
+	}
+	temp->next = newNode;
+	newNode->next = nullptr;
 }
 
+// Insert b/w two nodes -- NOT IMPLEMENTED
+void LinkedList::InsertBetween()
+{
+}
+
+void LinkedList::InsertAfterNode()
+{
+	int value(0);
+	cout << "Enter the value after which node to inserted ";
+	cin >> value;
+
+	node *temp = CreateNode(value);
+	node *p = start;
+	while (p != nullptr)
+	{
+		if (p->info == value)
+		{
+			temp->next = p->next;
+			p->next = temp;
+		}
+		p = p->next;
+	}
+}
+
+void LinkedList::InsertBeforeNode()
+{
+	int value(0);
+	cout << "Enter the value before which node to inserted ";
+	cin >> value;
+
+	node *temp = CreateNode(value);
+	if (start->info == value)
+	{
+		temp->next = start;
+		start = temp;
+	}
+	node *p = start;
+	while (p != nullptr)
+	{
+		if (p->info == value)
+		{
+			temp->next = p->next;
+			p->next = temp;
+		}
+		p = p->next;
+	}
+}
+
+void LinkedList::InsertAtPosition()
+{
+	int value(0);
+	cout << "Enter the value to be inserted at specific position " << endl;
+	cin >> value;
+
+	node *temp = CreateNode(value);
 
 
+}
 
 int main()
 {
