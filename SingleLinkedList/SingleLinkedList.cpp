778242bc4964a05af2f6df667d104c507d048370
Ankit Mishra <ankit1703@gmail.com>
2017-04-25 13:09:25 +0530

    Initial Commit

--- SingleLinkedList/SingleLinkedList.cpp
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -17,12 +17,48 @@ public:
 	{
 		start = nullptr;
 	}
-
+	node *CreateNode(int data);
+	void InsertAtBeg();
+	void InsertAtEnd();
 };
+
+node * LinkedList::CreateNode(int data)
+{
+	node *temp = new node();
+	temp->info = data;
+	temp->next = nullptr;
+	return temp;
+}
+
+void LinkedList::InsertAtBeg()
+{
+	int value(0);
+	cout << "Enter the value " << endl;
+	cin >> value;
+
+	node *temp = CreateNode(value);
+	if (temp == nullptr)
+	{
+		temp = start->next;
+
+	}
+
+
+
+
+}
+
+void LinkedList::InsertAtEnd()
+{
+
+}
+
+
 
 
 int main()
 {
     return 0;
 }
+
 
