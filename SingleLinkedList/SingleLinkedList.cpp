13c539a941530db72faadb3db79f1f9f7a68c629
Ankit Mishra <ankit1703@gmail.com>
2017-04-25 12:58:16 +0530

    Initial Commit

---
+++ SingleLinkedList/SingleLinkedList.cpp
@@ -0,0 +1,28 @@
+// SingleLinkedList.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+struct node
+{
+	int info;
+	node *next;
+}*start;
+
+class LinkedList
+{
+public:
+	LinkedList()
+	{
+		start = nullptr;
+	}
+
+};
+
+
+int main()
+{
+    return 0;
+}
+
