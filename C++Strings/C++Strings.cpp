bf110966c01979b7cbc555bc6b06d44ef466ee3b
Ankit Mishra <ankit1703@gmail.com>
2017-07-16 21:21:09 +0530

    All test case passed

--- C++Strings/C++Strings.cpp
+++ C++Strings/C++Strings.cpp
@@ -9,16 +9,19 @@ int main()
 {
 	int t(0);
 	cin >> t;
-	string str1, str2;
-	cin >> str1 >> str2;
+	while (t > 0)
+	{
+		string str1, str2;
+		cin >> str1 >> str2;
 
-	int strlen1 = str1.length();
-	int strlen2 = str2.length();
+		int strlen1 = str1.length();
+		int strlen2 = str2.length();
 
-	int size = (strlen1 > strlen2) ? strlen1 : strlen2;
-	cout << size << endl;
-	cout << str1 + str2 << endl;
-
+		int size = (strlen1 > strlen2) ? strlen1 : strlen2;
+		cout << size << endl;
+		cout << str1 + str2 << endl;
+		t--;
+	}
     return 0;
 }
 
