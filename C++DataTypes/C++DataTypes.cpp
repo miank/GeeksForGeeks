f17ac960d64e0acfc2aa34396b3310e1053c9768
Ankit Mishra <ankit1703@gmail.com>
2017-07-16 20:48:16 +0530

    All test case passed.

---
+++ C++DataTypes/C++DataTypes.cpp
@@ -0,0 +1,17 @@
+// C++DataTypes.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i(0);
+	char c;
+	float f(0.0f);
+
+	cin >> i >> c >> f;
+	cout << i << endl << c << endl << f;
+    return 0;
+}
+
