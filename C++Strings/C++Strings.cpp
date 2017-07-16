b4a4d56a01bcd6c6d8b8af4995d15c5201c510c7
Ankit Mishra <ankit1703@gmail.com>
2017-07-16 21:15:29 +0530

    Initial Commit

---
+++ C++Strings/C++Strings.cpp
@@ -0,0 +1,24 @@
+// C++Strings.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int t(0);
+	cin >> t;
+	string str1, str2;
+	cin >> str1 >> str2;
+
+	int strlen1 = str1.length();
+	int strlen2 = str2.length();
+
+	int size = (strlen1 > strlen2) ? strlen1 : strlen2;
+	cout << size << endl;
+	cout << str1 + str2 << endl;
+
+    return 0;
+}
+
