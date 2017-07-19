6df21c799694558a4563677809be0e68b38cf7d1
Ankit Mishra <ankit1703@gmail.com>
2017-07-19 21:47:03 +0530

    Revert "Revert "Initial Commit""
    This reverts commit a3d0281520d49aaf53cd2b4df00e7f1a75927c78.

---
+++ SecondLargest/SecondLargest.cpp
@@ -0,0 +1,30 @@
+// SecondLargest.cpp 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int t(0), num(0), ele(0), n(0);
+	cin >> t;
+	vector<int> ivec;
+
+	while (t > 0)
+	{
+		cin >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> ele;
+			ivec.push_back(ele);
+		}
+		std::sort(ivec.begin(), ivec.end());
+		cout << ivec[n - 2] << endl;
+		ivec.clear();
+		t--;
+	}
+	return 0;
+}
