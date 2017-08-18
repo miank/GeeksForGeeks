60c648f267f83f1032bf5c174a55126b1341b3d2
Ankit Mishra <ankit1703@gmail.com>
2017-08-18 16:39:34 +0530

    Initial Commit

---
+++ InfixToPostfix/InfixToPostfix.cpp
@@ -0,0 +1,89 @@
+// InfixToPostfix.cpp 
+
+#include "stdafx.h"
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+int prec(char str)
+{
+	if (str == '^')
+		return 3;
+	else if (str == '*' || str == '/')
+		return 2;
+	else if (str == '+' || str == '-')
+		return 1;
+	else
+		return -1;
+}
+
+string InfixToPostfix(string str)
+{
+	stack<char> strStack;
+	strStack.push('N');
+	int length = str.size();
+	string postfixStr;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+		{
+			postfixStr += str;
+		}
+		else if (str[i] == '(')
+		{
+			strStack.push('(');
+		}
+		else if (str[i] == ')')
+		{
+			while (strStack.top() != 'N' && strStack.top() != '(')
+			{
+				char c = strStack.top();
+				strStack.pop();
+				postfixStr += c;
+			}
+			if (strStack.top() == '(')
+			{
+				char c = strStack.top();
+				strStack.pop();
+			}			
+		}
+		else
+		{
+			while (strStack.top() != 'N' && prec(str[i]) <= prec(strStack.top()))
+			{
+				char c = strStack.top();
+				strStack.pop();
+				postfixStr += c;
+			}
+		}
+	}
+
+	while (strStack.top() != 'N')
+	{
+		char c = strStack.top();
+		strStack.pop();
+		postfixStr += c;
+	}
+	return postfixStr;
+}
+
+int main()
+{
+	int testCase(0);
+	cin >> testCase;
+
+	while (testCase > 0)
+	{
+		string str("");
+		cin >> str;
+		string s = InfixToPostfix(str);
+		cout << str << endl;
+		testCase--;
+	}
+
+    return 0;
+}
+
