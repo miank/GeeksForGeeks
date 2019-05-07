ad31fa159bc13a393894bead014e8d22a925e9f7
Ankit Mishra <ankit1703@hotmail.com>
2019-05-07 22:43:33 +0530

    Condition added to check for unecessary loop iterations.

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -15,9 +15,13 @@ int main()
 	{
 		cin >> arr[i];
 	}
+	cout << endl;
 	int temp(0);
+	bool swapped;
+
 	for (int i = 0; i < n - 1; i++)
 	{
+		swapped = false;
 		for (int j = 0; j < n - i - 1; j++)
 		{
 			if (arr[j] > arr[j + 1])
@@ -25,8 +29,13 @@ int main()
 				temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
+				swapped = true;
 			}
 		}
+		// If no two elements were swapped by inner loop then break
+		// It means array is sorted.
+		if (swapped == false)
+			break;
 	}
 	cout << "After sorting " << endl;
 	for (int i = 0; i < n; i++)
